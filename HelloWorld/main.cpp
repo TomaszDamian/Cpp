f664f317a0e8bef2e9841e93729a725aa319882f
Tomasz Damian Sidorowicz <FS459403@fss.is>
2018-01-09 10:08:23 +0000

    started to study cpp, for and if statements done

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a = 2;
+    float b = 3;
+    float c = a/b;
+    cout << c;
+    return 0;
+}
---
+++ input/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    //find largest number out of 4 (shitty if statement version)
+    int n1;
+    int n2;
+    int n3;
+    int n4;
+
+    cout << "Input your four number : ";
+    cin >> n1 >> n2 >> n3 >> n4;
+
+    if(n1 >= n2 && n1 >= n3 && n1 > n4)
+    {
+        cout << "Largest number: " << n1;
+    }
+
+    if(n2 >= n1 && n2 >= n3 && n2 > n4)
+    {
+        cout << "Largest number: " << n2;
+    }
+
+    if(n3 >= n1 && n3 >= n2 && n3 > n4) {
+        cout << "Largest number: " << n3;
+    }
+
+    if(n4 >= n1 && n4 >= n2 && n4 > n3) {
+        cout << "Largest number: " << n4;
+    }
+
+    //put in number and immidetly find out the sum
+    int Output = 0;
+    int InputNumber;
+    for(int i = 0; i <= 4; i++){
+        cout << "Input the number: ";
+        cin >> InputNumber;
+        Output = Output + InputNumber;
+        cout << Output << endl;
+        //cout << Output << endl;
+    };
+
+    //print from 10 down to 0
+    for(int i = 10; i > 0; i--){
+        cout << i << endl;
+    };*/
+}
